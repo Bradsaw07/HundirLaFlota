6634b3ec1616bbb213acc85990091722a8da7b18
Bradsaw07 <pablohalo10@gmail.com>
2024-10-10 22:26:57 +0200

    comentarios añadidos en algunas funciones

--- Tablero.py
+++ Tablero.py
@@ -1,15 +1,34 @@
 import random
 import string
 import tkinter as tk
+import pygame
 from Barcos import *
 
+#Inicializar Pygame
+pygame.init()
+
+#Cargar los sonidos
+disparo_sound = pygame.mixer.Sound("HundirLaFlota\Sonidos\cannonFire.wav")
+explosionWater = pygame.mixer.Sound("HundirLaFlota\Sonidos\explosionWater.wav")
+VictoryEasy = pygame.mixer.Sound("HundirLaFlota\Sonidos\Victory.wav")
+gameOverEasy = pygame.mixer.Sound("HundirLaFlota\Sonidos\GameOver.wav")
+VictoryNormal = pygame.mixer.Sound("HundirLaFlota\Sonidos\VictoryNormal.wav")
+YouLoseNormal = pygame.mixer.Sound("HundirLaFlota\Sonidos\youlose.wav")
+FlawlessVictory = pygame.mixer.Sound("HundirLaFlota\Sonidos\FlawlessVictory.wav")
+YouDie =pygame.mixer.Sound("HundirLaFlota\Sonidos\YouDied.wav")
+
+# Clase que representa un tablero de juego
 class Tablero:
     def __init__(self, filas, columnas):
+         # Inicializar las propiedades del tablero
         self.filas = filas
         self.columnas = columnas
+        # Crear una matriz vacía para representar el tablero
         self.casillas = [["." for _ in range(columnas)] for _ in range(filas)]
+        # Lista para almacenar las coordenadas atacadas
         self.coordenadas_atacadas = []
     
+    # Método para generar tableros de juego según la dificultad
     def generar_tableros(self, dificultad):
         if dificultad == 1:
             tablero_jugador = Tablero(10, 10)
@@ -22,8 +41,9 @@ class Tablero:
             tablero_rival = Tablero(20, 20)
 
         return tablero_jugador, tablero_rival
-
+    # Método para cargar barcos en el tablero
     def cargar_barcos(self, barcos, mostrar_barcos=False):
+        # Recorrer los barcos y marcar sus posiciones en el tablero
         for barco in barcos:
             for fila, columna in barco.posiciones:
                 if mostrar_barcos:
@@ -31,25 +51,34 @@ class Tablero:
                 else:
                     self.casillas[fila][columna] = "."
 
+    # Método para atacar una posición en el tablero
     def atacar(self, fila, columna, barcos_rival, mostrar_barcos=False):
+        # Verificar si la posición está dentro del tablero
         if fila < 0 or fila >= self.filas or columna < 0 or columna >= self.columnas:
             print("Coordenada fuera del tablero")
             return False
+        # Verificar si la posición ya ha sido atacada
         if (fila, columna) in self.coordenadas_atacadas:
             print("Coordenada ya atacada")
             return False
+        # Agregar la posición a la lista de coordenadas atacadas
         self.coordenadas_atacadas.append((fila, columna))
+        # Recorrer los barcos rivales y verificar si alguno ha sido atacado
         for barco in barcos_rival:
             if (fila, columna) in barco.posiciones:
+                # Marcar la posición como atacada
                 self.casillas[fila][columna] = "X"
                 if mostrar_barcos:
                     print("¡Barco hundido!")
+                     # Verificar si el barco ha sido completamente hundido
                 if all((fila, columna) in self.coordenadas_atacadas for fila, columna in barco.posiciones):
                     print("¡Barco completamente hundido!")
+                    # Eliminar el barco de la lista de barcos rivales
                     barcos_rival.remove(barco)
                 return True
-        self.casillas[fila][columna] = "."
+        self.casillas[fila][columna] = "A"
         print("¡Disparo fallido!")
+        explosionWater.play()
         return False
     
     def atacar_normal(self, fila, columna, barcos_rival, mostrar_barcos=False):
@@ -71,6 +100,7 @@ class Tablero:
                 return True
         self.casillas[fila][columna] = "A"
         print("¡Disparo fallido!")
+        explosionWater.play()
         return False
 
     def atacar_dificil(self, fila, columna, barcos_rival, mostrar_barcos=False):
@@ -92,6 +122,7 @@ class Tablero:
                 return True
         self.casillas[fila][columna] = "A"
         print("¡Disparo fallido!")
+        explosionWater.play()
         return False
 
     def imprimir_tablero(self):
@@ -115,6 +146,7 @@ class Tablero:
             for casilla in fila:
                 print(casilla, end=" ")
             print() 
+
 def mostrar_ventana_ganador_easy():
         ventana = tk.Tk()
         ventana.title("Felicidadees,¡Has ganadoooo!")
@@ -123,6 +155,7 @@ def mostrar_ventana_ganador_easy():
         label_imagen.pack()
         ventana.mainloop()
         
+        
 def mostrar_ventana_perdedor_easy():
         ventana = tk.Tk()
         ventana.title("¡Has perdido!")
@@ -130,6 +163,7 @@ def mostrar_ventana_perdedor_easy():
         label_imagen = tk.Label(ventana, image=imagen_perdedor)
         label_imagen.pack()
         ventana.mainloop()
+        
 
 def mostrar_ventana_ganador_normal():
         ventana = tk.Tk()
@@ -163,11 +197,13 @@ def mostrar_ventana_perdedor_dificil():
         label_imagen.pack()
         ventana.mainloop()
 
+    
+
 
 def jugar(dificultad,nombreId):
         tablero_jugador, tablero_rival = Tablero(0, 0).generar_tableros(dificultad)
-        barcos_jugador = Barco.crear_barcos(6, 3, "horizontal", tablero_jugador.filas, tablero_jugador.columnas)
-        barcos_rival = Barco.crear_barcos(6, 3, "horizontal", tablero_rival.filas, tablero_rival.columnas)
+        barcos_jugador = Barco.crear_barcos(5, 2, "horizontal", tablero_jugador.filas, tablero_jugador.columnas)
+        barcos_rival = Barco.crear_barcos(1, 2, "horizontal", tablero_rival.filas, tablero_rival.columnas)
         tablero_jugador.cargar_barcos(barcos_jugador)
         tablero_rival.cargar_barcos(barcos_rival, mostrar_barcos=True)
         while True:
@@ -180,9 +216,13 @@ def jugar(dificultad,nombreId):
                 columna = int(input("Ingrese la columna: "))
                 if tablero_rival.atacar(fila, columna, barcos_rival):
                     print("¡Disparo acertado!")
+                    disparo_sound.play()
                     if not barcos_rival:
                         print("¡Has ganado!")
+                        pygame.time.delay(2000)
+                        VictoryEasy.play()
                         mostrar_ventana_ganador_easy()
+                        
                         return
                 else:
                     print("¡Disparo fallido!")
@@ -195,7 +235,9 @@ def jugar(dificultad,nombreId):
                     print("¡La máquina ha acertado!")
                     if not barcos_jugador:
                         print("¡Has perdido!")
+                        pygame.time.delay(2000)
                         mostrar_ventana_perdedor_easy()
+                        gameOverEasy.play()
                         return
                 else:
                     print("¡La máquina ha fallado!")
@@ -219,7 +261,9 @@ def jugar_normal(dificultad, nombreId):
                     print("¡Disparo acertado!")
                     if not barcos_rival:
                         print("¡Has ganado!")
+                        pygame.time.delay(2000)
                         mostrar_ventana_ganador_normal()
+                        VictoryNormal.play()
                         return
                 else:
                     print("¡Disparo fallido!")
@@ -246,6 +290,8 @@ def jugar_normal(dificultad, nombreId):
                         print("¡La máquina ha acertado de nuevo!")
                         if not barcos_jugador:
                             print("¡Has perdido!")
+                            pygame.time.delay(2000)
+                            YouLoseNormal.play()
                             mostrar_ventana_perdedor_normal()
                             return
                     else:
@@ -276,6 +322,8 @@ def jugar_dificil(dificultad, nombreId):
                     print("¡Disparo acertado!")
                     if not barcos_rival:
                         print("¡Has ganado!")
+                        pygame.time.delay(2000)
+                        FlawlessVictory.play()
                         mostrar_ventana_ganador_dificil()
                         
                         return
@@ -307,6 +355,8 @@ def jugar_dificil(dificultad, nombreId):
                                 break
                             if not barcos_jugador:
                                 print("¡Has perdido!")
+                                pygame.time.delay(2000)
+                                YouDie.play()
                                 mostrar_ventana_perdedor_dificil()
                                 return
                             else:
